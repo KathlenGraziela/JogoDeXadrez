794706530f7799a2b06eac773da227268ef1fcce
Kathlen Graziela <kathlengraziela@gmail.com>
2022-11-26 14:16:57 -0300

    Comecando a implementar o tabuleiro

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -1,6 +1,4 @@
-﻿using Tabuleiro;
+﻿using tabuleiro;
 
-Posicao P;
-P = new Posicao(3, 4);
-Console.WriteLine("Posição: " + P);
+Tabuleiro tab = new Tabuleiro(8, 8);
 Console.ReadLine();
--- JogoDeXadrez/Tabuleiro/Posicao.cs
+++ JogoDeXadrez/Tabuleiro/Posicao.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-namespace Tabuleiro
+﻿namespace tabuleiro
 {
     class Posicao
     {
